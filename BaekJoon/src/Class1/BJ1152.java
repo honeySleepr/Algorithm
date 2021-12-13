d16c0dd45676452bbd9026e9cc9fc3644ef95eb7
honeySleepr <gruzzimo@naver.com>
2021-12-13 23:32:52 +0900

    알고리즘 Repository 로 이동
    기존 CodeSquadCocoa Repository 에 있던 알고리즘 코드들을 옮겼다

---
+++ BaekJoon/src/Class1/BJ1152.java
@@ -0,0 +1,19 @@
+package Class1;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BJ1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> stringList = List.of(sc.nextLine().split(" "));
+        int count = stringList.size();
+        for (String s : stringList) {
+            if(s.equals("")){
+                count--;
+                break;
+            }
+        }
+        System.out.println(count);
+        }
+    }
