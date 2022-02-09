d7b0f93b6f37cd5bb9fa249332b870fb32232b7a
honeySleepr <gruzzimo@naver.com>
2022-02-10 00:48:36 +0900

    (수정) 크로아티아 알파벳
    Map 대신 switch 문 사용하니 조금 더 빠름

--- BaekJoon/src/silver/BJ2941.java
+++ BaekJoon/src/silver/BJ2941.java
@@ -3,20 +3,11 @@ package silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+
+
 // 크로아티아 알파벳 (문자열)
 public class BJ2941 {
     public static void main(String[] args) throws IOException {
-        Map<String, Boolean> mapStr = new HashMap<>();
-        mapStr.put("c=", true);
-        mapStr.put("c-", true);
-        mapStr.put("dz=", true);
-        mapStr.put("d-", true);
-        mapStr.put("lj", true);
-        mapStr.put("nj", true);
-        mapStr.put("s=", true);
-        mapStr.put("z=", true);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -30,7 +21,7 @@ public class BJ2941 {
             sb.append(arrStr[i]);
 
             if (sb.length() == 3) {
-                if (mapStr.containsKey(sb.toString())) {
+                if (sb.toString().equals("dz=")) {
                     sb.setLength(0);
                 } else {
                     tempStr = sb.substring(1);
@@ -40,9 +31,20 @@ public class BJ2941 {
                 count++;
             }
 
-            if (sb.length() == 2 && mapStr.containsKey(sb.toString())) {
-                count++;
-                sb.setLength(0);
+            if (sb.length() == 2) {
+                switch (sb.toString()) {
+                    case "c=":
+                    case "c-":
+                    case "d-":
+                    case "lj":
+                    case "nj":
+                    case "s=":
+                    case "z=":
+                        count++;
+                        sb.setLength(0);
+                    default:
+                }
+
             }
         }
         count += sb.length();
